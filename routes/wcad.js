f53b885f19a9ad121c848f2ceb3b2c751810f804
Nick O <debuggeek@gmail.com>
2017-06-11 21:47:57 -0500

    Add equity response to table view

--- routes/wcad.js
+++ routes/wcad.js
@@ -8,6 +8,7 @@ var Improvement = require('../models/improvement.js');
 var Land = require('../models/wcad/tsgland.js');
 
 var wcadPropDao = require('../lib/wcadPropDao');
+var wcadFunc = require('../lib/wcadFunctions');
 
 router.get('/', cors(), function(req, res, next) {
   res.type('json');
@@ -49,42 +50,52 @@ router.get('/propertyByQuickRefId/:quickRefId', cors(), function(req, res, next)
 router.get('/comps/:quickRefId', cors(), function(req, res, next){
   const quickRefId = req.params['quickRefId'];
   const sqftRangePct = req.query['sqftRangePct'];
+  const salesComp = req.query['sales'];
 
   const percAboveBelow = sqftRangePct ? sqftRangePct : 10;
+  const useSales = salesComp ? salesComp : false;
   console.log('Finding comps for quickRefId=',quickRefId,
-              'at percAboveBelow=',percAboveBelow);
+              'at percAboveBelow=',percAboveBelow,
+              'salesComp=', useSales);
   const percAbove = 1 + (percAboveBelow / 100);
   const percBelow = 1 - (percAboveBelow / 100);
 
-  result = {"subject" : "", "comps" : ""};
+  let result = {"subject" : "","comps":[]};
 
+  let context = {"percAbove":percAbove,"percBelow":percBelow};
   wcadPropDao.getPropertyByQuickRefId(quickRefId)
-    .then(property => {
-      const subj = property.property;
-      console.log("Found subj=", subj.get('quickRefId'));
-      result.subject = subj;
-
-      const subjHood = subj.get('NeighborhoodCode');
-      const subjLivingArea = subj.get('TotalSqFtLivingArea');
-      Property.findAll({
-        where : {
-          NeighborhoodCode : subjHood,
-          TotalSqFtLivingArea : {
-            $and: {
-              $lt : subjLivingArea * percAbove,
-              $gt : subjLivingArea * percBelow
-            }
+    .then(wcadProp => {
+      result.subject = wcadProp.summary;
+      return wcadPropDao.getCompsFor(context, quickRefId)
+        .then( compList => {
+          var simpleList = [];
+          for(x in compList){
+            simpleList.push(compList[x].quickRefId);
           }
-        }
-      }).then(possibleComps => {
-        //console.log("PossibleComps = ", possibleComps.length);
-        //result.push({"comps" : possibleComps});
-        result.comps = possibleComps;
-        res.json(result);
-      })
-  }).catch(error => {
-      console.log("ERROR:", error);
-      res.status(500).send('Something broke!');
+          // console.log("simpleList:", simpleList);
+          wcadPropDao.getHydratedProps(simpleList)
+            .then( hydratedComps => {
+              let compSummaries = [];
+              for(x in hydratedComps){
+                compSummaries.push(hydratedComps[x].summary);
+              }
+              result.comps = compSummaries;
+              result = wcadFunc.calcDiffsForSales(result);
+              result = wcadFunc.calcIndicatedValue(result);
+              result = wcadFunc.sortComps(result, useSales);
+              res.json(result);
+            }).catch(error => {
+              console.log("HydrateError:", error);
+            })
+        });
+    }).catch(error => {
+      if(error.message === "no property found"){
+        console.log("INFO:", error.message);
+        res.status(404).send(error.message);
+      } else {
+        console.log("ERROR:", error);
+        res.status(500).send('Something broke!');
+      }
     });
 })
 
