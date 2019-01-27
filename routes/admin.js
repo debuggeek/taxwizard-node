6109e08c57e342f5977860939c81fe164dc82206
Nick O <debuggeek@gmail.com>
2019-01-27 15:13:31 -0600

    Add missing files from last commit

---
+++ routes/admin.js
@@ -0,0 +1,41 @@
+var mysql = require('mysql');
+const express = require("express");
+const router = express.Router();
+
+const db = require("../lib/db.js");
+const cfg = require("../lib/config").Config;
+
+var con = mysql.createConnection({
+    host     : cfg.host,
+    user     : cfg.user,
+    password : cfg.password,
+    database : cfg.database,
+    debug    : true
+});
+
+router.get("/verify", function(req, res) {
+    console.log("Executing verification");
+    // db.connection.query("SELECT COUNT(1) FROM `BATCH_PROP`")
+    //     .on('result', function (row) {
+    //         console.log("Connected");
+    //         res.json("Connected to db");
+    //         res.json(row['COUNT(1)']);
+    //     })
+    //     .on('error', function (err) {
+    //         res.status(500);
+    //         res.json(err);
+    //         //callback({error: true, err: err});
+    //     });
+    con.connect(function (err) {
+        if (err) {
+            res.send("Failed");
+            throw err;
+        } else {
+            console.log("Connected!");
+            res.send("Success");
+        }
+    });
+    //con.end();
+});
+
+module.exports = router;
