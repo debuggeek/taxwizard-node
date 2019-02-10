652b92d0cdcd67181524e381efbd5d6b2634b609
Nick O <debuggeek@gmail.com>
2019-02-10 15:07:17 -0600

    Add cognito layer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const CognitoExpress = require("cognito-express");
 
 const routes = require('./routes/index');
 const salesComp = require('./routes/salesComp');
@@ -24,6 +25,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Initializing CognitoExpress constructor
+const cognitoExpress = new CognitoExpress({
+  region: "us-west-2",
+  cognitoUserPoolId: "us-west-2_DTpJ5tgkq",
+  tokenUse: "access", //Possible Values: access | id
+  tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
+});
+
+//Our middleware that authenticates all APIs under our 'authenticatedRoute' Router
+app.use(function(req, res, next) {
+    
+  //I'm passing in the access token in header under key accessToken
+  let accessTokenFromClient = req.headers.accesstoken;
+
+  //Fail if token not present in header. 
+  if (!accessTokenFromClient) return res.status(401).send("Access Token missing from header");
+
+  cognitoExpress.validate(accessTokenFromClient, function(err, response) {
+      
+      //If API is not authenticated, Return 401 with error message. 
+      if (err) return res.status(401).send(err);
+      
+      //Else API has been authenticated. Proceed.
+      res.locals.user = response;
+      next();
+  });
+});
 
 app.use('/', routes);
 app.use('/salesComp', cors(), salesComp);
