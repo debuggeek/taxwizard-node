9961d73a65ca5d7dd611b5cb4e7ef13e8086b2d4
Nick O <debuggeek@gmail.com>
2019-03-09 15:16:21 -0600

    [ISSUE 35] Update WCAD for current versions to return table

--- routes/wcad.js
+++ routes/wcad.js
@@ -1,36 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var cors = require('cors')
+var cors = require("cors")
 
-var db = require('../lib/db.js');
-var Property = require('../models/property.js');
-var Improvement = require('../models/improvement.js');
-var Land = require('../models/wcad/tsgland.js');
+var db = require("../lib/db.js");
+var Property = require("../models/property.js");
+var Improvement = require("../models/improvement.js");
+var Land = require("../models/wcad/tsgland.js");
 
-var wcadPropDao = require('../lib/wcadPropDao');
-var wcadFunc = require('../lib/wcadFunctions');
+var wcadPropDao = require("../lib/wcadPropDao");
+var wcadFunc = require("../lib/wcadFunctions");
 
-router.get('/', cors(), function(req, res, next) {
-  res.type('json');
+router.get("/", cors(), function(req, res, next) {
+  res.type("json");
   res.send("OK");
 });
 
-router.get('/alive', cors(), function(req, res, next) {
+router.get("/alive", cors(), function(req, res, next) {
   db.sequelize
   .authenticate()
   .then(err => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
+    console.error("Unable to connect to the database:", err);
   });
-  res.type('json');
-  res.send('OK');
+  res.type("json");
+  res.send("OK");
 })
 
-router.get('/propertyByQuickRefId/:quickRefId', cors(), function(req, res, next) {
-  const quickRefId = req.params['quickRefId'];
-  console.log('Searching for', quickRefId);
+router.get("/propertyByQuickRefId/:quickRefId", cors(), function(req, res, next) {
+  const quickRefId = req.params["quickRefId"];
+  console.log("Searching for", quickRefId);
 
   wcadPropDao.getPropertyByQuickRefId(quickRefId)
     .then(wcadProp => {
@@ -42,15 +42,15 @@ router.get('/propertyByQuickRefId/:quickRefId', cors(), function(req, res, next)
         res.status(404).send(error.message);
       } else {
         console.log("ERROR:", error);
-        res.status(500).send('Something broke!');
+        res.status(500).send("Something broke!");
       }
     });
 })
 
-router.post('/comps/:quickRefId', cors(), function(req, res, next){
-    const quickRefId = req.params['quickRefId'];
-    const sqftRangePct = req.query['sqftRangePct'];
-    const salesComp = req.query['sales'];
+router.post("/comps/:quickRefId", cors(), function(req, res, next){
+    const quickRefId = req.params["quickRefId"];
+    const sqftRangePct = req.query["sqftRangePct"];
+    const salesComp = req.query["sales"];
     const useSales = salesComp ? salesComp : false;
     let percAbove = 0;
     let percBelow = 0;
@@ -102,61 +102,46 @@ router.post('/comps/:quickRefId', cors(), function(req, res, next){
             res.status(404).send(error.message);
         } else {
             console.log("ERROR:", error);
-            res.status(500).send('Something broke!');
+            res.status(500).send("Something broke!");
         }
     });
 })
 
-router.get('/comps/:quickRefId', cors(), function(req, res, next){
-  const quickRefId = req.params['quickRefId'];
-  const sqftRangePct = req.query['sqftRangePct'];
-  const salesComp = req.query['sales'];
+router.get("/comps/:quickRefId", cors(), async function(req, res, next){
+  const quickRefId = req.params["quickRefId"];
+  const sqftRangePct = req.query["sqftRangePct"];
+  const salesComp = req.query["sales"];
 
   const percAboveBelow = sqftRangePct ? sqftRangePct : 10;
   const useSales = salesComp ? salesComp : false;
-  console.log('Finding comps for quickRefId=',quickRefId,
-              'at percAboveBelow=',percAboveBelow,
-              'salesComp=', useSales);
+  console.log("Finding comps for quickRefId=",quickRefId,
+              "at percAboveBelow=",percAboveBelow,
+              "salesComp=", useSales);
   const percAbove = 1 + (percAboveBelow / 100);
   const percBelow = 1 - (percAboveBelow / 100);
 
   let result = {"subject" : "","comps":[]};
 
   let context = {"percAbove":percAbove,"percBelow":percBelow};
-  wcadPropDao.getPropertyByQuickRefId(quickRefId)
-    .then(wcadProp => {
-      result.subject = wcadProp.summary;
-      return wcadPropDao.getCompsFor(context, quickRefId)
-        .then( compList => {
-          let simpleList = [];
-          for(x in compList){
-            simpleList.push(compList[x].quickRefId);
-          }
-          console.log("simpleList:", simpleList);
-          wcadPropDao.getHydratedProps(simpleList)
-            .then( hydratedComps => {
-              let compSummaries = [];
-              for(x in hydratedComps){
-                compSummaries.push(hydratedComps[x].summary);
-              }
-              result.comps = compSummaries;
-              result = wcadFunc.calcDiffsForSales(result);
-              result = wcadFunc.calcIndicatedValue(result);
-              result = wcadFunc.sortComps(result, useSales);
-              res.json(result);
-            }).catch(error => {
-              console.log("HydrateError:", error);
-            })
-        });
-    }).catch(error => {
-      if(error.message === "no property found"){
-        console.log("INFO:", error.message);
-        res.status(404).send(error.message);
-      } else {
-        console.log("ERROR:", error);
-        res.status(500).send('Something broke!');
-      }
-    });
+  let wcadProp = await wcadPropDao.getPropertyByQuickRefId(quickRefId);
+    // .then(wcadProp => {
+  result.subject = wcadProp.summary;
+  let compList = await wcadPropDao.getCompsFor(context, result.subject);
+  let simpleList = [];
+  for(x in compList){
+    simpleList.push(compList[x].quickRefId);
+  }
+  // console.log("simpleList:", simpleList);
+  let hydratedComps = await wcadPropDao.getHydratedProps(simpleList);
+  let compSummaries = [];
+  for(x in hydratedComps){
+    compSummaries.push(hydratedComps[x].summary);
+  }
+  result.comps = compSummaries;
+  result = wcadFunc.calcDiffsForSales(result);
+  result = wcadFunc.calcIndicatedValue(result);
+  result = wcadFunc.sortComps(result, useSales);
+  res.json(result);
 })
 
 module.exports = router;
